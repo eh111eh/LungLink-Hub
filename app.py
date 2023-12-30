c908010ea2146e63a0e22d637c67ffb8ce587d55
eh111eh <97640870+eh111eh@users.noreply.github.com>
2023-12-30 13:58:37 +0000

    added feedback function

--- app.py
+++ app.py
@@ -4,26 +4,44 @@ from sqlalchemy import text
 
 app = Flask(__name__)
 
+
+# Function to load comments from the database
+def load_comments_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM comments"))
+    comments = [dict(zip(result.keys(), row)) for row in result.all()]
+    return comments
+
+
+# Function to save a new comment to the database
+def save_comment_to_db(comment):
+  with engine.connect() as conn:
+    conn.execute(text("INSERT INTO comments (comment) VALUES (:comment)"),
+                 comment=comment)
+
+
 def load_X_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from X"))
-        X = []
-        for row in result.all():
-            X.append(dict(zip(result.keys(), row)))
-        return X
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from X"))
+    X = []
+    for row in result.all():
+      X.append(dict(zip(result.keys(), row)))
+    return X
+
 
 @app.route("/")
 def hello_hacks():
-    X = load_X_from_db()
-    return render_template('home.html', jobs=X)
+  X = load_X_from_db()
+  return render_template('home.html', jobs=X)
+
 
 @app.route('/statistics')
 def statistics():
-    X = load_X_from_db()
-    return render_template('stat.html', X=X)
+  X = load_X_from_db()
+  return render_template('stat.html', X=X)
 
 
-@app.route("/api/stat")
+@app.route("/api/statistics")
 def list_stat():
   return jsonify(X)
 
