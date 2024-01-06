9adc5826630907dd85183907d9679f559ced923b
eh111eh <97640870+eh111eh@users.noreply.github.com>
2024-01-06 19:03:18 +0000

    modified home, ml, stat page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import engine
 from sqlalchemy import text
 import logging
@@ -6,7 +6,6 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-
 
 # Function to load X, countries, and years from the database
 def load_data_from_db():
@@ -22,12 +21,10 @@ def load_data_from_db():
 
   return X, countries, years
 
-
 @app.route("/")
 def hello_hacks():
-  X, _, _ = load_data_from_db()
-  return render_template('home.html', jobs=X)
-
+    X, _, _ = load_data_from_db()
+    return render_template('home.html', jobs=X)
 
 @app.route('/statistics')
 def statistics():
@@ -35,10 +32,46 @@ def statistics():
   return render_template('stat.html', X=X)
 
 
+@app.route('/area')
+def area():
+  X, _, _ = load_data_from_db()
+  return render_template('area.html', X=X)
+
+
+@app.route('/ml')
+def ml():
+  X, _, _ = load_data_from_db()
+  return render_template('ml.html', X=X)
+
+
+@app.route('/community')
+def community():
+  X, _, _ = load_data_from_db()
+  return render_template('community.html', X=X)
+
+
 @app.route("/api/statistics")
 def list_stat():
   return jsonify(X)
 
+
+@app.route("/api/area")
+def list_area():
+  return jsonify(X)
+
+
+@app.route("/api/ml")
+def list_ml():
+  return jsonify(X)
+
+
+@app.route("/api/community")
+def list_community():
+  return jsonify(X)
+
+@app.route('/')
+def index():
+    return render_template('your_html_file.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
